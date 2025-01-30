3f5b1b5e03e200eec96b8e29639d6d960845b5bd
MarthaLatorre <87924066+MarthaLatorre@users.noreply.github.com>
2025-01-30 03:58:10 -0500

    cambio config

--- cypress.config.js
+++ cypress.config.js
@@ -7,18 +7,8 @@ module.exports = defineConfig({
     },
     chromeWebSecurity: false,
     specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
-    experimentalStudio: true,
-    reporter: 'cypress-multi-reporters',
-    reporterOptions: {
-      reporterEnabled: 'mochawesome',
-      mochawesomeReporterOptions: {
-        reportDir: 'cypress/reports/mochawesome',
-        overwrite: false,
-        html: true,
-        json: true,
-        timestamp: 'mmddyyyy_HHMMss'
-      }
-    }
+    experimentalStudio: true
+    
   },
   /*env:{
     endpoint:{
